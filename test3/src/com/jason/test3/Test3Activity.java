6192307570673960b4e25690ca079a352cfb5340
jaesungPark <hotpjs1@gmail.com>
2012-06-02 16:58:22 +0900

    init test3

---
+++ test3/src/com/jason/test3/Test3Activity.java
@@ -0,0 +1,13 @@
+package com.jason.test3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Test3Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
